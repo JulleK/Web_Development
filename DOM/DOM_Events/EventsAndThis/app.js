f00948f9ba2d1987cef81c4f41b1e7e7e2ef293b
Julek <i@example.com>
2023-02-01 18:38:28 +0100

    Keyword this in Events

--- DOM/DOM_Events/EventsAndThis/app.js
+++ DOM/DOM_Events/EventsAndThis/app.js
@@ -5,18 +5,17 @@ const makeRandColor = () => {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-const buttons = document.querySelectorAll('button');
-
-for (let button of buttons) {
-    button.addEventListener('click', colorize)
-}
-
-const h1s = document.querySelectorAll('h1');
-for (let h1 of h1s) {
-    h1.addEventListener('click', colorize)
-}
-
 function colorize() {
     this.style.backgroundColor = makeRandColor();
     this.style.color = makeRandColor();
+}
+
+const buttons = document.querySelectorAll('button')
+for (let button of buttons) {
+    button.addEventListener('click', colorize);
+}
+
+const h1s = document.querySelectorAll('h1')
+for (let h1 of h1s) {
+    h1.addEventListener('click', colorize);
 }
