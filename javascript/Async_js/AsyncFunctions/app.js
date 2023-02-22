660f4e9cd3d05b34c3c02a61277543bd31a810ea
Julek <julek.krzeszycha@gmail.com>
2023-02-22 15:42:51 +0100

    js promises

--- javascript/Async_js/AsyncFunctions/app.js
+++ javascript/Async_js/AsyncFunctions/app.js
@@ -1,80 +1,58 @@
 // async function hello() {
 // }
 
+// const sing = async () => {
+//     throw 'oh ohho error'
+//     return 'lalalala'
+// }
 
-const sing = async () => {
-    throw "OH NO, PROBLEM!"
-    return 'LA LA LA LA'
-}
-
-sing()
-    .then(data => {
-        console.log("PROMISE RESOLVED WITH:", data)
-    })
-    .catch(err => {
-        console.log("OH NO, PROMISE REJECTED!")
-        console.log(err)
-    })
-
+// sing()
+//     .then(data => {
+//         console.log('promise fullfilled with:', data)
+//     })
+//     .catch(err => {
+//         console.log("oh no, promise rejected!")
+//         console.log(err)
+//     })
 
 
+// const login = async (username, password) => {
+//     if (!username || !password) throw 'Missing Credentials'
+//     if (password === 'cutecorgi') return 'Welcome!'
+//     throw 'Invalid Password'
+// }
 
-const login = async (username, password) => {
-    if (!username || !password) throw 'Missing Credentials'
-    if (password === 'corgifeetarecute') return 'WELCOME!'
-    throw 'Invalid Password'
-}
-
-login('todd', 'corgifeetarecute')
-    .then(msg => {
-        console.log("LOGGED IN!")
-        console.log(msg)
-    })
-    .catch(err => {
-        console.log("ERROR!")
-        console.log(err)
-    })
-
-
-
-
-
+// login('asdasd', 'cutecorgi')
+//     .then(msg => {
+//         console.log("Logged In!")
+//         console.log(msg)
+//     })
+//     .catch(err => {
+//         console.log("Error!")
+//         console.log(err)
+//     })
 
 const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             document.body.style.backgroundColor = color;
             resolve();
-        }, delay)
+        }, delay);
     })
 }
-
-// delayedColorChange('red', 1000)
-//     .then(() => delayedColorChange('orange', 1000))
-//     .then(() => delayedColorChange('yellow', 1000))
-//     .then(() => delayedColorChange('green', 1000))
-//     .then(() => delayedColorChange('blue', 1000))
-//     .then(() => delayedColorChange('indigo', 1000))
-//     .then(() => delayedColorChange('violet', 1000))
-
 
 async function rainbow() {
     await delayedColorChange('red', 1000)
     await delayedColorChange('orange', 1000)
     await delayedColorChange('yellow', 1000)
-    await delayedColorChange('green', 1000)
-    await delayedColorChange('blue', 1000)
-    await delayedColorChange('indigo', 1000)
-    await delayedColorChange('violet', 1000)
-    return "ALL DONE!"
+    return "ALL DONE"
 }
 
-// rainbow().then(() => console.log("END OF RAINBOW!"))
-
+// rainbow().then(() => console.log("END OF RAINBOW"))
 
 async function printRainbow() {
     await rainbow();
-    console.log("END OF RAINBOW!")
+    console.log("END OF RAINBOW")
 }
 
 printRainbow();
@@ -103,5 +81,4 @@ async function makeTwoRequests() {
         console.log("CAUGHT AN ERROR!")
         console.log("error is:", e)
     }
-
 }
--- javascript/Async_js/PromisesIntro/app.js
+++ javascript/Async_js/PromisesIntro/app.js
@@ -9,6 +9,7 @@ const fakeRequestCallback = (url, success, failure) => {
         }
     }, delay)
 }
+
 // THE PROMISE VERSION 
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
@@ -22,60 +23,6 @@ const fakeRequestPromise = (url) => {
         }, delay)
     })
 }
-
-
-// fakeRequestCallback('books.com/page1',
-//     function (response) {
-//         console.log("IT WORKED!!!!")
-//         console.log(response)
-//         fakeRequestCallback('books.com/page2',
-//             function (response) {
-//                 console.log("IT WORKED AGAIN!!!!")
-//                 console.log(response)
-//                 fakeRequestCallback('books.com/page3',
-//                     function (response) {
-//                         console.log("IT WORKED AGAIN (3rd req)!!!!")
-//                         console.log(response)
-//                     },
-//                     function (err) {
-//                         console.log("ERROR (3rd req)!!!", err)
-//                     })
-//             },
-//             function (err) {
-//                 console.log("ERROR (2nd req)!!!", err)
-//             })
-//     }, function (err) {
-//         console.log("ERROR!!!", err)
-//     })
-
-
-
-
-
-
-
-// fakeRequestPromise('yelp.com/api/coffee/page1')
-//     .then(() => {
-//         console.log("IT WORKED!!!!!! (page1)")
-//         fakeRequestPromise('yelp.com/api/coffee/page2')
-//             .then(() => {
-//                 console.log("IT WORKED!!!!!! (page2)")
-//                 fakeRequestPromise('yelp.com/api/coffee/page3')
-//                     .then(() => {
-//                         console.log("IT WORKED!!!!!! (page3)")
-//                     })
-//                     .catch(() => {
-//                         console.log("OH NO, ERROR!!! (page3)")
-//                     })
-//             })
-//             .catch(() => {
-//                 console.log("OH NO, ERROR!!! (page2)")
-//             })
-//     })
-//     .catch(() => {
-//         console.log("OH NO, ERROR!!! (page1)")
-//     })
-
 
 // THE CLEANEST OPTION WITH THEN/CATCH
 // RETURN A PROMISE FROM .THEN() CALLBACK SO WE CAN CHAIN!
@@ -98,6 +45,3 @@ fakeRequestPromise('yelp.com/api/coffee/page1')
         console.log("OH NO, A REQUEST FAILED!!!")
         console.log(err)
     })
-
-
-
