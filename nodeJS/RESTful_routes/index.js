3302d731f0a003cf0bff45b83b48be61b82d9219
Julek <julek.krzeszycha@gmail.com>
2023-03-19 12:17:48 +0100

    RESTful routes comment project finished

--- nodeJS/RESTful_routes/index.js
+++ nodeJS/RESTful_routes/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -56,6 +58,18 @@ app.patch('/comments/:id', (req, res) => {
     const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
     foundComment.comment = newCommentText;
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id)
     res.redirect('/comments')
 })
 
