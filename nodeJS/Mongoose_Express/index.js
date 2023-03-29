a63018e89a35bf7c6baba4a8a2e5b27fb11f062e
Julek <julek.krzeszycha@gmail.com>
2023-03-29 19:18:48 +0200

    products index & details routes

--- nodeJS/Mongoose_Express/index.js
+++ nodeJS/Mongoose_Express/index.js
@@ -17,12 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-
-
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
-    console.log(products)
-    res.send("ALL PRODUCTS WILL BE HERE")
+    res.render('products/index', { products })
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/details', { product })
 })
 
 app.listen(3000, () => {
