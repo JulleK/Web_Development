00aea7295c3c8581c752f1d33b79010100f0e49e
Julek <julek.krzeszycha@gmail.com>
2023-03-29 18:56:21 +0200

    seeded some data into db

--- nodeJS/Mongoose_Express/index.js
+++ nodeJS/Mongoose_Express/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express()
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product')
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/dog', (req, res) => {
-    res.send("WOOOF!")
+
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    console.log(products)
+    res.send("ALL PRODUCTS WILL BE HERE")
 })
 
 app.listen(3000, () => {
