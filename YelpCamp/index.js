2aa82e8116a4366db1079c1821c1abe93edc90ea
Julek <julek.krzeszycha@gmail.com>
2023-04-01 17:21:37 +0200

    campgrounds index route. seeded some camps into db

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const path = require('path');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
 
-// mongoose.connect('mongodb://localhost:27017/yelp-camp')
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log("MongoDB Connected!")
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
 const app = express();
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+
+
+
+
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({})
+    console.log(campgrounds)
+    res.render('campgrounds/index', { campgrounds })
 })
 
 app.listen(3000, () => {
