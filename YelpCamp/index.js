0d721f8a4d306d7c9f6909c597dbb139c8cc3d90
Julek <julek.krzeszycha@gmail.com>
2023-04-02 13:51:41 +0200

    Yelp Camp new/create route

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 
 
 
@@ -26,13 +27,22 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({})
-    console.log(campgrounds)
     res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(3000, () => {
