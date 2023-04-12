3a9c0883b8a2ef3fe7b04abed56781d71d8a200a
Julek <julek.krzeszycha@gmail.com>
2023-04-12 16:51:17 +0200

    added some images + reseeded db with a new model

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -12,15 +12,26 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log(err)
     })
 
+let description = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga labore,
+voluptatibus totam doloremque aperiam dolor deleniti praesentium hic esse quod
+quos commodi quisquam, doloribus vel illum aspernatur expedita rerum similique!
+Omnis rem praesentium eius magnam, eveniet dolorum dicta provident doloremque
+quod totam alias, culpa, voluptatem beatae? Beatae quia, enim aliquam
+repellendus placeat ea omnis laborum at aliquid. Dolores, minima incidunt.`
+
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description,
+            price
         })
         await camp.save()
     }
