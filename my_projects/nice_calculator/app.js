2eb4697e73fea16c84ae7f445bdaca3c3caee04c
Julek <julek.krzeszycha@gmail.com>
2023-04-30 19:45:03 +0200

    .

--- my_projects/nice_calculator/app.js
+++ my_projects/nice_calculator/app.js
@@ -1,4 +1,5 @@
-const screen = document.querySelector('.screen')
+const firstScreenNum = document.querySelector('.first-screen-num')
+const secondScreenNum = document.querySelector('.second-screen-num')
 const buttons = document.querySelectorAll('.calc-button')
 const operatorDisplay = document.querySelector('.operator-display')
 
@@ -7,6 +8,7 @@ let total = 0;
 let newNum = 0;
 let currentOperator; // operators: + - x / C ←
 
+// some button clicked
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         buttonValue = button.innerText
@@ -14,16 +16,29 @@ buttons.forEach(button => {
     })
 })
 
+// button clicked handler
 function buttonClick(buttonValue) {
     if (isNaN(buttonValue)) {
         handleOperator(buttonValue)
     } else {
+        handleNum()
+    }
+}
+
+function handleNum() {
+    if (!currentOperator) {
         if (!newNum)
             newNum = buttonValue
         else
             newNum += buttonValue
-        console.log(newNum)
-        screen.innerText = newNum
+        firstScreenNum.innerText = newNum
+    }
+    else {
+        if (!newNum)
+            newNum = buttonValue
+        else
+            newNum += buttonValue
+        secondScreenNum.innerText = newNum
     }
 }
 
@@ -32,25 +47,33 @@ function handleOperator(operator) {
         currentOperator = operator
         operatorDisplay.innerText = currentOperator
     }
+    newNum = 0
     switch (operator) {
         case "+":
             changeDisplayedOperator()
+            total = firstScreenNum.innerText + secondScreenNum.innerText
             break;
 
         case "-":
             changeDisplayedOperator()
+            total -= newNum
             break;
 
         case "x":
             changeDisplayedOperator()
+            total *= newNum
             break;
 
         case "÷":
             changeDisplayedOperator()
+            total /= newNum
             break;
 
         case "=":
-            // TODO
+            firstScreenNum.innerText = `${total}`
+            secondScreenNum.innerText = ""
+            currentOperator = null;
+            console.log(total)
             break;
 
         case "C":
@@ -58,7 +81,8 @@ function handleOperator(operator) {
             operatorDisplay.innerText = ""
             total = 0
             newNum = 0
-            screen.innerText = "0"
+            firstScreenNum.innerText = "0"
+            secondScreenNum.innerText = ""
             break;
 
         case "←":
