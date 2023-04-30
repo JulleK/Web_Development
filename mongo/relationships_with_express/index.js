68d14995e331e557783849b8e814ee81a17850c4
Julek <julek.krzeszycha@gmail.com>
2023-04-30 20:34:53 +0200

    data relationships with express demo done

--- mongo/relationships_with_express/index.js
+++ mongo/relationships_with_express/index.js
@@ -28,6 +28,11 @@ app.get('/farms', async (req, res) => {
     res.render('farms/index', { farms })
 })
 
+app.delete('/farms/:id', async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id)
+    res.redirect('/farms')
+})
+
 app.get('/farms/new', (req, res) => {
     res.render('farms/new')
 })
@@ -39,13 +44,14 @@ app.post('/farms', async (req, res) => {
 })
 
 app.get('/farms/:id', async (req, res) => {
-    const farm = await Farm.findById(req.params.id)
+    const farm = await Farm.findById(req.params.id).populate('products')
     res.render('farms/show', { farm })
 })
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
     const { id } = req.params
-    res.render('products/new', { categories, id })
+    const farm = await Farm.findById(id)
+    res.render('products/new', { categories, farm })
 })
 
 app.post('/farms/:id/products', async (req, res) => {
@@ -57,7 +63,7 @@ app.post('/farms/:id/products', async (req, res) => {
     product.farm = farm;
     await farm.save()
     await product.save()
-    res.send(farm)
+    res.redirect(`/farms/${id}`)
 })
 
 
@@ -88,7 +94,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
-    const product = await Product.findById(id)
+    const product = await Product.findById(id).populate('farm', 'name')
     res.render('products/details', { product })
 })
 
--- mongo/relationships_with_express/models/farm.js
+++ mongo/relationships_with_express/models/farm.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Product = require('./product')
 const { Schema } = mongoose
 
 const farmSchema = new Schema({
@@ -19,6 +20,13 @@ const farmSchema = new Schema({
             ref: 'Product'
         }
     ]
+});
+
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = await Product.deleteMany({ _id: { $in: farm.products } })
+        console.log(res)
+    }
 })
 
 const Farm = mongoose.model('Farm', farmSchema);
