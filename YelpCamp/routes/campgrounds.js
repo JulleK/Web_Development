ecf5ec71f2c0f4a2830c437edba12d44b049a90e
Julek <julek.krzeszycha@gmail.com>
2023-05-23 15:40:08 +0200

    moved routes out of main index.js file

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
+
 const Campground = require('../models/campground')
 const { campgroundSchema } = require('../schemas.js')
+
+const catchAsync = require('../utils/catchAsync')
+const ExpressError = require('../utils/ExpressError')
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body)
