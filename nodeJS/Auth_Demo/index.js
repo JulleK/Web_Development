55098f6f0eb9e8f06c4980c3fbb53fc94b253954
Julek <julek.krzeszycha@gmail.com>
2023-05-27 17:59:52 +0200

    implemented login

--- nodeJS/Auth_Demo/index.js
+++ nodeJS/Auth_Demo/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express();
 const User = require('./models/user')
 const bcrypt = require('bcrypt')
+const session = require('express-session')
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'notagoodsecret' }))
 
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE')
@@ -34,10 +36,34 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save()
+    req.session.user_id = user._id
     res.redirect('/')
 })
 
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username })
+    if (!user) {
+        return res.redirect('/login')
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (validPassword) {
+        req.session.user_id = user._id
+        res.redirect('/secret')
+    } else {
+        res.redirect('/login')
+    }
+})
+
 app.get('/secret', (req, res) => {
+    if (!req.session.user_id) {
+        res.redirect('/login')
+    }
     res.send('THIS IS SECRET! YOU CANNOT SEE ME IF YOU ARE NOT LOGGED IN');
 });
 
