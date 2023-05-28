94d64aa00f7ee99f12a1792b00580b7eb9101844
Julek <julek.krzeszycha@gmail.com>
2023-05-28 19:11:32 +0200

    basic configuration of passport

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -6,6 +6,9 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
 const ExpressError = require('./utils/ExpressError')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
@@ -40,6 +43,13 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser)
+passport.deserializeUser(User.deserializeUser)
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
