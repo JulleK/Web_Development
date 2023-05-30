2a7ff84b9e12f8492e5741e63d5d5a1cc7b056cd
Julek <julek.krzeszycha@gmail.com>
2023-05-30 16:10:29 +0200

    added campground - author connection

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -27,6 +27,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6475fb938217abeb1f03765f',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
