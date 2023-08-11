2e26c44a8c83e11b61e755e5d29b759f6308c8a8
Julek <julek.krzeszycha@gmail.com>
2023-08-11 14:45:14 +0200

    added wpm counting, page nearly finished

--- my_projects/typing_test/app.js
+++ my_projects/typing_test/app.js
@@ -1,9 +1,14 @@
 const testText = document.querySelector('#test-text')
 const userInput = document.querySelector('#test-user-input')
 const timerScreen = document.querySelector('#timer-clock')
+const screen = document.querySelector('#screen')
+const resultsScreen = document.querySelector('#results-screen')
+const wpmResult = document.querySelector('#wpm')
 
-let userMistakes = 0
+let totalCorrect = 0
+let testLetterCount = 0
 let testStarted = false
+let testTime = 30
 
 const words = [
     "pioneer",
@@ -37,18 +42,19 @@ for (word of words) {
 }
 
 testText.firstChild.classList.add('active')
+timerScreen.innerText = testTime
 
-userInput.addEventListener('input', () => {
+userInput.addEventListener('input', (evt) => {
     if (!testStarted) {
-        countdownTimer(timerScreen, 15)
-
+        countdownTimer(timerScreen, testTime)
         testStarted = true
     }
+
     const activeWord = document.querySelector(".active")
     const inputText = userInput.value
 
     // check if user made mistakes in current word
-    highlightWrong(activeWord, inputText)
+    highlightWrong(activeWord, inputText, evt)
 
 
     // if user presses space, go to next word 
@@ -56,22 +62,25 @@ userInput.addEventListener('input', () => {
     goToNextWord(activeWord, inputText)
 })
 
-function highlightWrong(activeWord, inputText) {
+function highlightWrong(activeWord, inputText, evt) {
     // if user input !== active word
     if (inputText !== activeWord.innerText.slice(0, inputText.length)) {
-        // if the word isn't highlighted as wrong add 1 mistake
-        if (!activeWord.classList.contains('highlight-wrong')) {
-            userMistakes += 1
-        }
         activeWord.classList.add('highlight-wrong')
-    } else {
-        // correct input = remove the class
+    }
+    // on correct input remove highlighting
+    else {
         activeWord.classList.remove('highlight-wrong')
     }
 }
 
 function goToNextWord(activeWord, inputText) {
+
     if (inputText[inputText.length - 1] === " ") {
+        // when user wants to go to next word
+        //  firstly count how many letters he got right
+        countCorrectLetters(activeWord.innerText.trim(), inputText.trim())
+
+        // if user typed the word correctly
         if (inputText.trimStart() === activeWord.innerText) {
             activeWord.classList.add('correct')
         }
@@ -87,6 +96,15 @@ function goToNextWord(activeWord, inputText) {
     }
 }
 
+function countCorrectLetters(activeWord, inputText) {
+    for (let i = 0; i < inputText.length; i++) {
+        if (activeWord[i] === inputText[i]) {
+            totalCorrect += 1
+        }
+    }
+    testLetterCount += inputText.length
+}
+
 function countdownTimer(timerScreen, seconds) {
     // function counting down from n seconds to 0
     //  I will use it as timer for the test
@@ -95,7 +113,24 @@ function countdownTimer(timerScreen, seconds) {
     const intervalId = setInterval(() => {
         i -= 1
         timerScreen.innerText = i
-        if (i === 0)
+
+        // timer goes down to 0
+        if (i === 0) {
             clearInterval(intervalId)
+
+            showResults()
+        }
     }, 1000)
+}
+
+function showResults() {
+    screen.style.display = "none"
+    resultsScreen.style.display = "inherit"
+
+    // calculate wpm
+    const timeInMinutes = testTime / 60
+    const wpm = ((totalCorrect / 5) - ((testLetterCount - totalCorrect) * timeInMinutes)) / timeInMinutes
+
+    // on wpm less than 0 - assign wpm to 0
+    wpm > 0 ? wpmResult.innerText = wpm : wpmResult.innerText = 0
 }
