ed6a7c9484a66a885ab11dbd5636d2b9904eb5c7
Julek <julek.krzeszycha@gmail.com>
2023-08-27 20:39:02 +0200

    fixed the cluster container select

--- YelpCamp/public/js/clusterMap.js
+++ YelpCamp/public/js/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoianVsbGVrIiwiYSI6ImNsbGY5MnVsaTB4djkzcXFoeXZyb3lnbmIifQ.tnbEwTwdGfsJiJRRSc7lWQ';
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/light-v11',
     center: [-103.5917, 40.6699],
