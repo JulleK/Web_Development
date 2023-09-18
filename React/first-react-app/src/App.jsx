8545dfb6588b42600fb081fb2ef95c83d4d8ba5a
Julek <julek.krzeszycha@gmail.com>
2023-09-18 18:44:41 +0200

    conditionals with react

--- React/first-react-app/src/App.jsx
+++ React/first-react-app/src/App.jsx
@@ -2,11 +2,12 @@ import RandomPokemon from "./RandomPokemon.jsx";
 import Greeter from "./Greeter.jsx";
 import Die from "./Die.jsx";
 import ListPicker from "./ListPicker.jsx";
+import DoubleDice from "./DoubleDice.jsx";
 
 export default function App() {
   return (
     <div className="App">
-      <ListPicker values={[1, 2, 3]} />
+      {/* <ListPicker values={[1, 2, 3]} /> */}
       {/* <Greeter person="Bob" from="Colt" />
       <RandomPokemon /> */}
       {/* <Die numSides={20} />
@@ -14,6 +15,7 @@ export default function App() {
       <Die numSides={2} />
       <Die numSides={1} />
       <Die /> */}
+      <DoubleDice />
     </div>
   );
 }
---
+++ React/first-react-app/src/DoubleDice.jsx
@@ -0,0 +1,37 @@
+export default function DoubleDice() {
+  const num1 = Math.floor(Math.random() * 3) + 1;
+  const num2 = Math.floor(Math.random() * 3) + 1;
+  return (
+    <div>
+      <h2>Double Dice!</h2>
+      {num1 === num2 && <h3>You Win!</h3>}
+      <p>Num1 = {num1}</p>
+      <p>Num2 = {num2}</p>
+    </div>
+  );
+}
+
+// export default function DoubleDice() {
+//   const num1 = Math.floor(Math.random() * 3) + 1;
+//   const num2 = Math.floor(Math.random() * 3) + 1;
+//   return (
+//     <div>
+//       <h2>Double Dice!</h2>
+//       {num1 === num2 ? <h3>You Win!</h3> : null}
+//       <p>Num1 = {num1}</p>
+//       <p>Num2 = {num2}</p>
+//     </div>
+//   );
+// }
+
+// export default function DoubleDice() {
+//   const num1 = Math.floor(Math.random() * 3) + 1;
+//   const num2 = Math.floor(Math.random() * 3) + 1;
+//   return (
+//     <div>
+//       <p>{num1 === num2 ? "You Win!" : "You Lose :("}</p>
+//       <p>Num1 = {num1}</p>
+//       <p>Num2 = {num2}</p>
+//     </div>
+//   );
+// }
