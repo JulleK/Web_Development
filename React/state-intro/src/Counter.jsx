35759bb29f994290ea7ae6f7ae47c8577f1619b3
Julek <julek.krzeszycha@gmail.com>
2023-09-29 21:17:07 +0200

    useState hook basics

--- React/state-intro/src/Counter.jsx
+++ React/state-intro/src/Counter.jsx
@@ -1,13 +1,13 @@
-export default function Counter() {
-  let num = 0;
-  const incrementNum = () => {
-    num++;
-    console.log(num);
-  };
+import { useState } from "react";
 
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <div>
-      <button onClick={incrementNum}>The count is: {num}</button>
+      <button onClick={handleClick}>The count is: {count}</button>
     </div>
   );
 }
