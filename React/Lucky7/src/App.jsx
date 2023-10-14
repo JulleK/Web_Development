a482fb0caff06fcd14b7e33b115787840b5d784a
Julek <julek.krzeszycha@gmail.com>
2023-10-14 21:42:26 +0200

    started lucky 7 mini-project

---
+++ React/Lucky7/src/App.jsx
@@ -0,0 +1,28 @@
+import Lucky7 from "./Lucky7";
+import LuckyN from "./LuckyN";
+import { sum } from "./utils";
+import BoxGrid from "./BoxGrid";
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function allSameValue(dice) {
+  return dice.every((v) => v === dice[0]);
+}
+
+function App() {
+  return (
+    <>
+      <Lucky7 />
+      {/* <BoxGrid /> */}
+      {/* <LuckyN winCheck={lessThan4} title="Roll less than 4" />
+      <LuckyN
+        winCheck={allSameValue}
+        numDice={3}
+        title="Roll the same number"
+      /> */}
+    </>
+  );
+}
+
+export default App;
