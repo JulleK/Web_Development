eab1609fb95f78303280493b7daf1b540634a356
Julek <julek.krzeszycha@gmail.com>
2023-10-20 21:11:14 +0200

    Passing Functions That Update State

--- React/Lucky7/src/LuckyN.jsx
+++ React/Lucky7/src/LuckyN.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { getRolls, sum } from "./utils";
 import Dice from "./Dice";
+import Button from "./Button";
 
 export default function LuckyN({
   numDice = 2,
@@ -16,7 +17,8 @@ export default function LuckyN({
         {title} {isWinner && "You Win!"}
       </h1>
       <Dice dice={dice} />
-      <button onClick={roll}>Re-Roll Dice</button>
+      {/* <button onClick={roll}>Re-Roll Dice</button> */}
+      <Button clickFunc={roll} label="Re-Roll" />
     </main>
   );
 }
