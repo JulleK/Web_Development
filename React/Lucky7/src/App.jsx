e2967b5c141c9e182250f90c657f703240a01c3b
Julek <julek.krzeszycha@gmail.com>
2023-10-20 20:55:13 +0200

    passing functions as props

--- React/Lucky7/src/App.jsx
+++ React/Lucky7/src/App.jsx
@@ -1,22 +1,27 @@
 import Lucky7 from "./Lucky7";
 import Dice from "./Dice";
 import LuckyN from "./LuckyN";
-// import { sum } from "./utils";
+import { sum } from "./utils";
 // import BoxGrid from "./BoxGrid";
 // function lessThan4(dice) {
 //   return sum(dice) < 4;
 // }
 
-// function allSameValue(dice) {
-//   return dice.every((v) => v === dice[0]);
-// }
+function allSameValue(dice) {
+  return dice.every((v) => v === dice[0]);
+}
+
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
 
 function App() {
   return (
     <>
       {/* <Lucky7 /> */}
       {/* <Dice dice={[5, 1, 2, 6, 5]} /> */}
-      <LuckyN numDice={4} goal={12} />
+      <LuckyN numDice={1} winCheck={lessThan4} title="Roll Less Than 4" />
+      <LuckyN winCheck={allSameValue} title="Roll The Same Number" />
 
       {/* <BoxGrid /> */}
       {/* <LuckyN winCheck={lessThan4} title="Roll less than 4" />
--- React/Lucky7/src/LuckyN.jsx
+++ React/Lucky7/src/LuckyN.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import { getRolls, sum } from "./utils";
 import Dice from "./Dice";
 
-export default function LuckyN({ numDice = 2, goal = 7 }) {
+export default function LuckyN({
+  numDice = 2,
+  winCheck = (dice) => sum(dice) === 7,
+  title = "Lucky Dice",
+}) {
   const [dice, setDice] = useState(getRolls(numDice));
-  const isWinner = sum(dice) === goal;
   const roll = () => setDice(getRolls(numDice));
+  const isWinner = winCheck(dice);
   return (
     <main>
       <h1>
-        Lucky{goal} {isWinner && "You Win!"}
+        {title} {isWinner && "You Win!"}
       </h1>
       <Dice dice={dice} />
       <button onClick={roll}>Re-Roll Dice</button>
