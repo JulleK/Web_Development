4bdd00529f299f3e967f642b00d98f4bec08c6cb
Julek <julek.krzeszycha@gmail.com>
2023-10-22 18:52:13 +0200

    controlled components and forms

---
+++ React/forms/src/App.jsx
@@ -0,0 +1,12 @@
+import UsernameForm from "./UsernameForm";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <UsernameForm />
+    </>
+  );
+}
+
+export default App;
---
+++ React/forms/src/UsernameForm.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+export default function UsernameForm() {
+  const [username, setUsername] = useState("");
+  function updateUsername(evt) {
+    console.log(evt);
+    setUsername(evt.target.value);
+  }
+  return (
+    <div>
+      <label htmlFor="">Enter a username</label>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={updateUsername}
+        id="username"
+      />
+      <button>Submit</button>
+    </div>
+  );
+}
