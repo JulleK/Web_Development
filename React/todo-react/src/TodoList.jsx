e1a11ce74358d1c43638c782967b7d3e222d85b9
Julek <julek.krzeszycha@gmail.com>
2023-11-14 18:20:50 +0100

    toggle todo checkbox and remove todo functionality

--- React/todo-react/src/TodoList.jsx
+++ React/todo-react/src/TodoList.jsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-// import IconButton from "@mui/material/IconButton";
+import TodoItem from "./TodoItem";
 
 const initialTodos = [
   { id: 1, text: "walk the dog", completed: false },
@@ -16,37 +11,30 @@ const initialTodos = [
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
-  const handleToggle = (todo) => {
-    setTodos({ ...todo, completed: !todo.completed });
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id != id);
+    });
+  };
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else return todo;
+      });
+    });
   };
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-
         return (
-          <ListItem
+          <TodoItem
+            todo={todo}
             key={todo.id}
-            //   secondaryAction={
-            //     <IconButton edge="end" aria-label="comments">
-            //       <CommentIcon />
-            //     </IconButton>
-            //   }
-            disablePadding
-          >
-            <ListItemButton role={undefined} dense>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todo.text} />
-            </ListItemButton>
-          </ListItem>
+            toggle={() => toggleTodo(todo.id)}
+            remove={() => removeTodo(todo.id)}
+          />
         );
       })}
     </List>
