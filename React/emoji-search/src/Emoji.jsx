cb75247d51f46208110976b44e33ed9bbded6513
Julek <julek.krzeszycha@gmail.com>
2023-12-08 18:26:25 +0100

    fixed emoji onClick copy to clipboard bug

--- React/emoji-search/src/Emoji.jsx
+++ React/emoji-search/src/Emoji.jsx
@@ -1,7 +1,7 @@
 export default function Emoji({ emoji, title, notify }) {
   const onClick = () => {
     // copies an emoji to clipboard on click
-    navigator.clipboard.writeText(emoji.symbol);
+    navigator.clipboard.writeText(emoji);
 
     // notify displays "copied to clipboard"
     notify();
