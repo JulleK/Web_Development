107bdd24e336d28e307696c2ac4adb423a7b6312
Julek <julek.krzeszycha@gmail.com>
2023-12-08 17:08:45 +0100

    formatted the js script

--- my_projects/weather_app/app.js
+++ my_projects/weather_app/app.js
@@ -2,54 +2,58 @@
 // api key: cf6a9e772a821a6992f85a5864771ed3
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=
 
-const apiKey = 'cf6a9e772a821a6992f85a5864771ed3';
+const apiKey = "cf6a9e772a821a6992f85a5864771ed3";
 
-const cityForm = document.querySelector('#cityForm')
-const cityInput = document.querySelector('#cityInput')
-const temperature = document.querySelector('#temperature')
-const weatherImg = document.querySelector('#weatherImg')
-const cityButton = document.querySelector('#cityButton')
-const weatherDescription = document.querySelector('#description')
-const cityName = document.querySelector('#cityName')
+const cityForm = document.querySelector("#cityForm");
+const cityInput = document.querySelector("#cityInput");
+const temperature = document.querySelector("#temperature");
+const weatherImg = document.querySelector("#weatherImg");
+const cityButton = document.querySelector("#cityButton");
+const weatherDescription = document.querySelector("#description");
+const cityName = document.querySelector("#cityName");
 
-cityForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let searchedCity = cityInput.value
-    searchCityCoordinates(searchedCity)
-    disableButton()
-})
+cityForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let searchedCity = cityInput.value;
+  searchCityCoordinates(searchedCity);
+  disableButton();
+});
 
 const searchCityCoordinates = async (searchedCity) => {
-    try {
-        const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${apiKey}`);
-        const data = await res.json();
-        let lat = data[0].lat
-        let lon = data[0].lon
-        searchWeather(lat, lon)
-    }
-    catch (e) {
-        console.log("ERROR!! I DON'T KNOW THIS CITY!", e)
-        alert("Something went wrong! please try again with different city name.")
-    }
-}
+  try {
+    const res = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity}&appid=${apiKey}`
+    );
+    const data = await res.json();
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+    searchWeather(lat, lon);
+  } catch (e) {
+    console.log("ERROR!! I DON'T KNOW THIS CITY!", e);
+    alert("Something went wrong! please try again with different city name.");
+  }
+};
 
 const searchWeather = async (lat, lon) => {
-    try {
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-        const data = await res.json();
-        console.log(data)
-        temperature.innerText = Math.round(data.main.temp) + "°C"
-        weatherImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        weatherDescription.innerText = data.weather[0].description
-        cityName.innerText = data.name
-    }
-    catch (e) {
-        console.log("SOMETHING WENT WRONG!", e)
-        alert("Something went wrong! please try again with different city name.")
-    }
-}
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    );
+    const data = await res.json();
+    console.log(data);
+    temperature.innerText = Math.round(data.main.temp) + "°C";
+    weatherImg.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherDescription.innerText = data.weather[0].description;
+    cityName.innerText = data.name;
+  } catch (e) {
+    console.log("SOMETHING WENT WRONG!", e);
+    alert("Something went wrong! please try again with different city name.");
+  }
+};
 
 function disableButton() {
-    cityButton.disabled = true;
-    setTimeout(function () { cityButton.disabled = false; }, 1500);
+  cityButton.disabled = true;
+  setTimeout(function () {
+    cityButton.disabled = false;
+  }, 1500);
 }
