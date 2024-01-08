808746cc095744fb5857586ac3c26e6ea5bd07f7
Julek <julek.krzeszycha@gmail.com>
2024-01-08 18:18:56 +0100

    create directory with new js features

---
+++ javascript/New_Features/console_table.js
@@ -0,0 +1,8 @@
+const foo = { name: "tom", age: 30, nervous: false };
+const bar = { name: "dick", age: 40, nervous: false };
+const baz = { name: "harry", age: 50, nervous: true };
+
+console.log("%c My Friends", "color: orange; font-weight: bold;");
+console.log({ foo, bar, baz });
+
+// console.table([foo, bar, baz]);
