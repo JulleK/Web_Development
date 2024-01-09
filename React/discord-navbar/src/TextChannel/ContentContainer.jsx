e4af970b645b272d205e9ac6f9ea8e14ee877784
Julek <julek.krzeszycha@gmail.com>
2024-01-09 19:28:14 +0100

    render a bunch of posts; css to do

--- React/discord-navbar/src/TextChannel/ContentContainer.jsx
+++ React/discord-navbar/src/TextChannel/ContentContainer.jsx
@@ -1,9 +1,70 @@
 import TopNavigation from "../TopNavigation/TopNavigation";
+import Post from "./Post";
+import BottomBar from "./BottomBar";
 
 export default function ContentContainer() {
   return (
     <div className="content-container">
       <TopNavigation />
+      <div className="content-list">
+        <Post
+          name="Ada"
+          timestamp="one week ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name="Leon"
+          timestamp="one week ago"
+          text={`Lorem ipsum dolor. `}
+        />
+        <Post name="Jill" timestamp="5 days ago" text={`Lorem.`} />
+        <Post
+          name="Ellie"
+          timestamp="4 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
+        />
+        <Post
+          name="Chris"
+          timestamp="4 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name="Claire"
+          timestamp="2 days ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
+        />
+        <Post
+          name="Albert"
+          timestamp="22 hours ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. ☺️ `}
+        />
+        <Post
+          name="Rebecca"
+          timestamp="3 hours ago"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.`}
+        />
+        <Post
+          name="H.U.N.K"
+          timestamp="Just now"
+          text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit.`}
+        />
+      </div>
+      <BottomBar />
     </div>
   );
 }
