d168e81a32c556fd1f4f33b98675ed10917c6b51
Julek <julek.krzeszycha@gmail.com>
2024-01-20 20:32:34 +0100

    update post styles

--- React/discord-navbar/src/TextChannel/Post.jsx
+++ React/discord-navbar/src/TextChannel/Post.jsx
@@ -3,13 +3,13 @@ import Avatar from "boring-avatars";
 export default function Post({ name, timestamp, text }) {
   return (
     <div className="post">
-      <div className="avatar-wrapper">
+      <div className="post-avatar">
         <Avatar size={30} name={name} variant="beam" />
       </div>
       <div className="post-content">
-        <p className="post-owner">
-          {name}
-          <small className="timestamp">{timestamp}</small>
+        <p className="post-info">
+          <span className="post-owner">{name}</span>
+          <small className="post-timestamp">{timestamp}</small>
         </p>
         <p className="post-text">{text}</p>
       </div>
