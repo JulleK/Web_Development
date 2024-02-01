96bb3ea4cd42df432d033145b48fd370f7356e75
Julek <julek.krzeszycha@gmail.com>
2024-02-01 15:54:10 +0100

    create basic about me page

--- React/About-Me/src/About.jsx
+++ React/About-Me/src/About.jsx
@@ -1,3 +1,37 @@
 export default function About() {
-  return <div></div>;
+  return (
+    <div className="mx-auto mt-14 text-center text-3xl">
+      <div className="mb-3">
+        <img
+          className="mx-auto w-40 rounded-full"
+          src="/src/images/me2.jpg"
+          alt="cool photo of JulleK"
+        />
+      </div>
+
+      <div className="border-b-2 pb-8 text-3xl">
+        <p>Hi, my name is Julian, I am from Poland 🇵🇱</p>
+        <p>I am 18 years old and I love Programming❤</p>
+        <br />
+        <p>I create web applications, </p>
+        <p>
+          <a
+            className="text-aqua-400 underline hover:text-aqua-600"
+            href="#technologies"
+          >
+            here
+          </a>{" "}
+          you can see the technologies I use
+        </p>
+      </div>
+      <div className="mt-20">
+        <h3 className="text-5xl font-bold underline">My Interests</h3>
+      </div>
+      <div className="mt-40">
+        <h3 className="font-bold" id="technologies">
+          Technologies I Use
+        </h3>
+      </div>
+    </div>
+  );
 }
