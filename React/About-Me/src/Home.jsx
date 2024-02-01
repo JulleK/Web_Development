c74586249fb622d04d8d311cf7e70d24d711882b
Julek <julek.krzeszycha@gmail.com>
2024-02-01 15:53:38 +0100

    refactor home page styles

--- React/About-Me/src/Home.jsx
+++ React/About-Me/src/Home.jsx
@@ -2,18 +2,19 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div className="mx-10 mt-32 text-center text-4xl md:mt-40 lg:mt-60 xl:mt-80">
+      <div className="mx-10 mt-32 text-center text-4xl md:mt-40 lg:mt-60 xl:mt-72">
         <p>
           Hi! My name is <span className=" text-aqua-400">Julian.</span>
         </p>
-        <p>I am 18 years old and am very happy to see you on my page!</p>
+        <p>I am very happy to see you on my page!</p>
       </div>
-      <div className=" mt-10 text-center text-5xl">
+      <div className="sm:3xl mt-14 text-center text-2xl md:text-5xl">
         <Link
           to="/about"
-          className="hover:border-aqua-600 border-aqua-500
-           hover:text-aqua-500 text-aqua-400 rounded-xl border-4
-            p-2 transition-all duration-150 focus-within:rounded-3xl"
+          className="rounded-xl border-4
+           border-aqua-500 p-2 text-aqua-400 transition-all
+            duration-150 focus-within:rounded-3xl
+           hover:border-aqua-600 hover:text-aqua-500"
         >
           Get To Know Me Better!
         </Link>
