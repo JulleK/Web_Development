8df7868d655015363780e5f7ab607e691e62cf5d
Julek <julek.krzeszycha@gmail.com>
2024-02-09 15:40:24 +0100

    change homepage styles

--- React/About-Me/src/Home.jsx
+++ React/About-Me/src/Home.jsx
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <div className="mx-10 mt-32 text-center text-4xl md:mt-40 lg:mt-60 xl:mt-72">
+      <div className="mx-10 mt-32 text-center text-4xl md:mt-64">
         <p>
           Hi! My name is <span className=" text-aqua-400">Julian.</span>
         </p>
         <p>I am very happy to see you on my page!</p>
       </div>
-      <div className="sm:3xl mt-14 text-center text-2xl md:text-5xl">
-        <Link
-          to="/about"
-          className="rounded-xl border-4
-           border-aqua-500 p-2 text-aqua-400 transition-all
-            duration-150 focus-within:rounded-3xl
-           hover:border-aqua-600 hover:text-aqua-500"
-        >
+      <div
+        className="sm:3xl mx-auto mt-32 w-fit rounded-xl
+          border-4 border-aqua-500 p-3 text-center
+           text-4xl text-aqua-400 transition-all
+          duration-300 focus-within:rounded-3xl
+          hover:scale-110 hover:border-aqua-300
+          hover:text-aqua-200 md:text-5xl"
+      >
+        <Link to="/about" className="">
           Get To Know Me Better!
         </Link>
       </div>
