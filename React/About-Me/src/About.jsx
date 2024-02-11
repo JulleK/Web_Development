6a94acbb3a1c7d99e0eba32f3a9085c8116222e4
Julek <julek.krzeszycha@gmail.com>
2024-02-11 21:52:00 +0100

    change line 18 mb; swap anchor tag to link

--- React/About-Me/src/About.jsx
+++ React/About-Me/src/About.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import InterestsShort from "./InterestsShort";
+import { Link } from "react-router-dom";
 
 export default function About() {
   // with this state we add a
@@ -14,7 +15,7 @@ export default function About() {
       className="mx-auto mt-14 text-center text-3xl transition-all duration-700"
       style={{ opacity: isLoading ? 0 : 1 }}
     >
-      <div className="mb-3">
+      <div className="mb-5">
         <img
           className="mx-auto w-40 rounded-full"
           src="/src/images/me2.jpg"
@@ -27,24 +28,18 @@ export default function About() {
         <p>I am 18 years old and I love Programming❤</p>
         <br />
         <p>I create web applications, </p>
-        <p>
-          <a
+        <p className="whitespace-pre">
+          <Link
             className="text-aqua-400 underline hover:text-aqua-600"
             href="#technologies"
           >
             here
-          </a>{" "}
+          </Link>{" "}
           you can see the technologies I use
         </p>
       </div>
 
       <InterestsShort />
-
-      <div className="mt-40">
-        <h3 className="font-bold" id="technologies">
-          Technologies I Use
-        </h3>
-      </div>
     </div>
   );
 }
