ec3788ceb6a2bb070ebe01e2c93b980aa20ed80a
Julek <julek.krzeszycha@gmail.com>
2024-03-21 21:48:18 +0100

    get route for book; put route to update book; book delete route

--- MERN_stack/book_store/backend/index.js
+++ MERN_stack/book_store/backend/index.js
@@ -36,6 +36,7 @@ app.post("/books", async (req, res) => {
   }
 });
 
+// Route to show all books
 app.get("/books", async (req, res) => {
   try {
     const books = await Book.find({});
@@ -43,6 +44,61 @@ app.get("/books", async (req, res) => {
       count: books.length,
       data: books,
     });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to show 1 book
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    console.log(typeof id);
+
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to update 1 book
+app.put("/books/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book updated successfully" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to delete 1 book
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      res.status(404).json({ message: "Book not found" });
+    }
+
+    return res.status(200).send({ message: "Book deleted successfully" });
   } catch (err) {
     console.log(err.message);
     res.status(500).send({ message: err.message });
