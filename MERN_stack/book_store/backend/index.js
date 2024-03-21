32eb3a9a9058a8d5c161e396578b6b01b77775a1
Julek <julek.krzeszycha@gmail.com>
2024-03-21 18:20:40 +0100

    /books get route

--- MERN_stack/book_store/backend/index.js
+++ MERN_stack/book_store/backend/index.js
@@ -36,6 +36,19 @@ app.post("/books", async (req, res) => {
   }
 });
 
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
 mongoose
   .connect(mongodbUrl)
   .then(() => {
