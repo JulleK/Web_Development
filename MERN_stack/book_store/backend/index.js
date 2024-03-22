46fd33c817837cebc904acc436467c28c49e9f32
Julek <julek.krzeszycha@gmail.com>
2024-03-22 18:13:45 +0100

    move book routes to router file; install and configure cors

--- MERN_stack/book_store/backend/index.js
+++ MERN_stack/book_store/backend/index.js
@@ -1,109 +1,33 @@
-import express, { response } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongodbUrl } from "./config.js";
-import { Book } from "./models/bookModel.js";
+import bookRouter from "./routes/booksRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 // parse the data
-app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Middleware for handling CORS Policy
+// so that react will have access to our server
+// Option 1: allow all origins
+// app.use(cors());
+// Option 2: Allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   return res.send("WELCOME WELCOME!!!");
 });
 
-// Route to show all books
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
-
-// Route to create a new book
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-
-    return res.status(201).send(book);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
-
-// Route to show 1 book
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    console.log(typeof id);
-
-    const book = await Book.findById(id);
-    return res.status(200).json(book);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
-
-// Route to update 1 book
-app.put("/books/:id", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-    const { id } = req.params;
-
-    const result = await Book.findByIdAndUpdate(id, req.body);
-
-    if (!result) {
-      res.status(404).json({ message: "Book not found" });
-    }
-
-    return res.status(200).send({ message: "Book updated successfully" });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
-
-// Route to delete 1 book
-app.delete("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const result = await Book.findByIdAndDelete(id);
-    if (!result) {
-      res.status(404).json({ message: "Book not found" });
-    }
-
-    return res.status(200).send({ message: "Book deleted successfully" });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send({ message: err.message });
-  }
-});
+app.use("/books", bookRouter);
 
 mongoose
   .connect(mongodbUrl)
