4b50c08882946e8c1365d4dc28c292e5dd64b071
Julek <julek.krzeszycha@gmail.com>
2024-03-23 20:33:07 +0100

    change cors policy to allow all origins

--- MERN_stack/book_store/backend/index.js
+++ MERN_stack/book_store/backend/index.js
@@ -13,15 +13,15 @@ app.use(express.json());
 // Middleware for handling CORS Policy
 // so that react will have access to our server
 // Option 1: allow all origins
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow custom origins
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (req, res) => {
   return res.send("WELCOME WELCOME!!!");
