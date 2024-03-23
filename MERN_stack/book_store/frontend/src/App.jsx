f15fad7d74c5c47b57ee5dea443fe4ca8d1c02ca
Julek <julek.krzeszycha@gmail.com>
2024-03-23 20:33:30 +0100

    add routes

--- MERN_stack/book_store/frontend/src/App.jsx
+++ MERN_stack/book_store/frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+
 function App() {
   return (
     <Routes>
-      <Route path="" element />
-      <Route path="" element />
-      <Route path="" element />
-      <Route path="" element />
-      <Route path="" element />
-      <div className="bg-red-400 text-white">Hello from React!!!!</div>;
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
     </Routes>
   );
 }
