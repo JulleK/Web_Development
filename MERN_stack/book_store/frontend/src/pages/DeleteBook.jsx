222b5675e592590d88f599fa80a17df8818891cb
Julek <julek.krzeszycha@gmail.com>
2024-03-25 18:06:48 +0100

    make books delete page

--- MERN_stack/book_store/frontend/src/pages/DeleteBook.jsx
+++ MERN_stack/book_store/frontend/src/pages/DeleteBook.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 function DeleteBook() {
-  return <div>DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(`http://localhost:3000/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("An error happened. Please check console");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading && <Spinner />}
+      <div
+        className="flex flex-col items-center border-2 border-sky-400 
+      rounded-xl max-md:w-[350px] w-[600px] p-8 mx-auto"
+      >
+        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+
+        <button
+          className="p-4 bg-red-600 text-white m-8 mb-2 w-full"
+          onClick={handleDeleteBook}
+        >
+          Delete The Book
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default DeleteBook;
