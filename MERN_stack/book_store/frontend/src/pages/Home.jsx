479514f4f87a0a5e4528eafba781630e76af9ca5
Julek <julek.krzeszycha@gmail.com>
2024-03-25 18:14:21 +0100

    toggle between table and card books display

--- MERN_stack/book_store/frontend/src/pages/Home.jsx
+++ MERN_stack/book_store/frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import BooksTable from "../components/BooksTable";
 function Home() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [showType, setShowType] = useState("table");
 
   useEffect(() => {
     setLoading(true);
@@ -24,13 +25,33 @@ function Home() {
   }, []);
   return (
     <div className="p-4">
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-600 hover:bg-sky-700 px-4 py-1 rounded-lg"
+          onClick={() => setShowType("table")}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-600 hover:bg-sky-700 px-4 py-1 rounded-lg"
+          onClick={() => setShowType("card")}
+        >
+          Card
+        </button>
+      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Books</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-600 text-5xl" />
         </Link>
       </div>
-      {loading ? <Spinner /> : <BooksTable books={books} />}
+      {loading ? (
+        <Spinner />
+      ) : showType === "table" ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
     </div>
   );
 }
