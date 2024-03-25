17e6bdc3cb04f8d60f82e7d9aea9e94652063005
Julek <julek.krzeszycha@gmail.com>
2024-03-25 18:27:08 +0100

    add navigation icon hover color

--- MERN_stack/book_store/frontend/src/components/BooksTable.jsx
+++ MERN_stack/book_store/frontend/src/components/BooksTable.jsx
@@ -37,13 +37,13 @@ function BooksTable({ books }) {
             <td className="border border-slate-700 rounded-md text-center">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`}>
-                  <BsInfoCircle className="text-2xl text-green-700" />
+                  <BsInfoCircle className="text-2xl text-green-700 hover:text-black" />
                 </Link>
                 <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600" />
+                  <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
                 </Link>
                 <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className="text-2xl text-red-600" />
+                  <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
                 </Link>
               </div>
             </td>
