d273e7041bae4c362806cb7017887a7f0972a5a4
Julek <julek.krzeszycha@gmail.com>
2024-03-25 17:51:01 +0100

    adjust create book form for smaller screens

--- MERN_stack/book_store/frontend/src/pages/CreateBook.jsx
+++ MERN_stack/book_store/frontend/src/pages/CreateBook.jsx
@@ -36,7 +36,10 @@ function CreateBook() {
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading && <Spinner />}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+      <div
+        className="flex flex-col border-2 border-sky-400
+       rounded-xl max-md:w-[300px] w-[600px] p-4 mx-auto"
+      >
         <div className="my-4">
           <label htmlFor="title" className="text-xl mr-4 text-gray-500">
             Title
