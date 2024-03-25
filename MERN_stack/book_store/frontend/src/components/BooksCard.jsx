59b1994e1c5d41e95bc86c6f517d646a1f4e0f17
Julek <julek.krzeszycha@gmail.com>
2024-03-25 18:29:36 +0100

    change navigation flex justify

--- MERN_stack/book_store/frontend/src/components/BooksCard.jsx
+++ MERN_stack/book_store/frontend/src/components/BooksCard.jsx
@@ -29,7 +29,7 @@ function BooksCard({ books }) {
             <BiUserCircle className="text-red-300" />
             <h2 className="my-1">{item.author}</h2>
           </div>
-          <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+          <div className="flex justify-evenly items-center gap-x-2 mt-4 p-4">
             <Link to={`/books/details/${item._id}`}>
               <BsInfoCircle className="text-2xl text-green-700 hover:text-black" />
             </Link>
