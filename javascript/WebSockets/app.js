a59d4ad50d91e0904e53b8abc5215dc095cfe42a
Julek <julek.krzeszycha@gmail.com>
2024-04-13 21:38:30 +0200

    websocket open and message events

--- javascript/WebSockets/app.js
+++ javascript/WebSockets/app.js
@@ -1,3 +1,14 @@
+// https://javascript.info/websocket
 let socket = new WebSocket(
   "wss://javascript.info/article/websocket/demo/hello"
 );
+
+socket.addEventListener("open", () => {
+  console.log("Connection estabilished");
+  console.log("Sending to server");
+  socket.send("My name is Johny");
+});
+
+socket.addEventListener("message", (evt) => {
+  console.log(`%c${evt.data}`, "color: cyan");
+});
