539833a4ede9d45ea963171aef32641aeb8e65f4
Julek <julek.krzeszycha@gmail.com>
2024-04-15 20:25:18 +0200

    todo: javascript websockets client and server

--- javascript/WebSockets/app.js
+++ javascript/WebSockets/app.js
@@ -1,30 +1,32 @@
-// https://javascript.info/websocket
-let socket = new WebSocket(
-  "wss://javascript.info/article/websocket/demo/hello"
-);
+// // https://javascript.info/websocket
+// let socket = new WebSocket(
+//   "wss://javascript.info/article/websocket/demo/hello"
+// );
 
-socket.addEventListener("open", () => {
-  console.log("Connection estabilished");
-  console.log("Sending to server");
-  socket.send("My name is Johny");
-});
+// socket.addEventListener("open", () => {
+//   console.log("Connection estabilished");
+//   console.log("Sending to server");
+//   socket.send("My name is Johny");
+// });
 
-socket.addEventListener("message", (event) => {
-  console.log(`%c${event.data}`, "color: cyan");
-});
+// socket.addEventListener("message", (event) => {
+//   console.log(`%c${event.data}`, "color: cyan");
+// });
 
-socket.addEventListener("close", (event) => {
-  if (event.wasClean === true) {
-    console.log(
-      `%c[close] connection closed cleanly, code=${event.code}, reason="${event.reason}"`,
-      "color: orange"
-    );
-  } else {
-    console.log(`%c[close] connection died!`, "color: red");
-  }
-});
+// socket.addEventListener("close", (event) => {
+//   if (event.wasClean === true) {
+//     console.log(
+//       `%c[close] connection closed cleanly, code=${event.code}, reason="${event.reason}"`,
+//       "color: orange"
+//     );
+//   } else {
+//     console.log(`%c[close] connection died!`, "color: red");
+//   }
+// });
 
-socket.addEventListener("error", (error) => {
-  console.log("%c[error] an error occured", "color: red");
-  console.error(error);
-});
+// socket.addEventListener("error", (error) => {
+//   console.log("%c[error] an error occured", "color: red");
+//   console.error(error);
+// });
+
+// ------------------------------------------------------
