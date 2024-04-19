a9b0a5ea9209270604226f19c08d880c6436d717
Julek <julek.krzeszycha@gmail.com>
2024-04-19 18:50:42 +0200

    remove unnescessary console.log

--- nodeJS/Websockets/server.js
+++ nodeJS/Websockets/server.js
@@ -1,7 +1,6 @@
 import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 wss.on("connection", (ws) => {
-  console.log(ws);
   ws.on("error", console.error);
 
   ws.on("message", (data) => {
