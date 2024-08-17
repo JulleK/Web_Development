0f470619d70dae5e60bd7df6ae2da8a5edb71ca1
Julek <julek.krzeszycha@gmail.com>
2024-08-17 15:52:07 +0200

    control flow exercises

--- Rust/Basics/functions/src/main.rs
+++ Rust/Basics/functions/src/main.rs
@@ -1,21 +1,39 @@
 fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
+    // exercises
+    // - Convert temperatures between Fahrenheit and Celsius.
+    // - Generate the nth Fibonacci number.
 
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+    // 1.
+    let fahrenheit: f64 = 34.0;
+    print!("{}°F = {}°C", fahrenheit, fahrenheit_to_celsius(fahrenheit));
 
-        count += 1;
+    print!("\n");
+
+    let celsius: f64 = 1.0;
+    print!("{}°C = {}°F", celsius, celsius_to_fahrenheit(celsius));
+
+    print!("\n");
+
+    // 2.
+    print!("{}", fibbonacci(4));
+}
+
+// 1.
+fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    return (fahrenheit - 32.0) / 1.8;
+}
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    return (celsius * 1.8) + 32.0;
+}
+
+// 2.
+fn fibbonacci(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    } else if n == 1 {
+        return 1;
+    } else {
+        return fibbonacci(n - 1) + fibbonacci(n - 2);
     }
-    println!("End count = {count}");
 }
