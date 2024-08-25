3f4870e080d277134d06c228a9981e27fabe189a
Julek <julek.krzeszycha@gmail.com>
2024-08-25 19:47:17 +0200

    string slice type

--- Rust/Basics/ownership/src/main.rs
+++ Rust/Basics/ownership/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    change(&mut s);
-
-    println!("{}", s);
+    let s = String::from("Hello World");
+    let word = first_word(&s);
+    println!("{}", word)
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
